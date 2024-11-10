0af549ea55c9ccfa536ef38fe5981f9062899077
mohammedgidrah <m7mgidrah@gmail.com>
2024-11-10 13:43:08 +0300

    added the logo and fiew images

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -17,14 +17,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="style.css">
-    <link rel="icon" type="image/png" href="assets/img/home/masterpeace_logo-removebg-preview.png" />
+    <link rel="icon" type="image/jpg" href="images/logo.jpg" />
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
         href="https://fonts.googleapis.com/css2?family=Montserrat:wght@900&family=Roboto:ital,wght@0,400;0,700;1,400;1,700&display=swap"
         rel="stylesheet">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
 </head>
 
@@ -32,7 +32,7 @@
 
     <div class="preloader"></div>
 
-    <div class="nav nav-overlay">
+    <div class="nav nav-overlay" >
         <div class="nav__content">
             <div class="container">
                 <div class="row p-4 p-md-0 flex-column-reverse flex-md-row g-md-5 nav__block">
@@ -50,24 +50,27 @@
                     </div> --}}
                     <div class="col-md-4 text-white">
                         <h3 class="text-white fw-bold nav__block-item">Contact info</h3>
- 
+
                         <ul class="list-unstyled text-light nav__block-item">
                             <li class="menu-item">
-                                <i class="icon icon-location me-2"></i>العقبة - دوار هيا - عمارة الملكية - الطابق الأول
+                                <i class="icon icon-location me-2" id="nav_icone"></i>العقبة - دوار هيا - عمارة الملكية
+                                - الطابق الأول
                             </li>
                             <li class="menu-item">
-                                <i class="icon icon-location2 me-2"></i>Aqaba-Haya Circle-RJ Building-1st Floor
+                                <i class="icon icon-location2 me-2" id="nav_icone"></i>Aqaba-Haya Circle-RJ Building-1st
+                                Floor
                             </li>
                             <li class="menu-item">
-                                <i class="icon icon-phone me-2"></i>+962 79 084 6327
+                                <i class="icon icon-phone me-2" id="nav_icone"></i>+962 79 084 6327
                             </li>
                             <li class="menu-item">
-                                <i class="icon icon-envelope-o me-2"></i><a href="#"
+                                <i class="icon icon-envelope-o me-2" id="nav_icone"></i><a
+                                    href="mailto:Lanaalmanaseerr@gmail.com"
                                     class="text-white">Lanaalmanaseerr@gmail.com</a>
                             </li>
                         </ul>
                     </div>
- 
+
                 </div>
             </div>
 
@@ -77,8 +80,8 @@
         <li><a href="#about">asfvfdcw</a></li>
     </ul> --}}
 
-    <div class="main-logo">
-        <a href="index.html">A.</a>
+    <div class="main-logo" id="home">
+        <a href="#" ><img class="logo" src="images/logo-removebg-preview.png" alt=""></a>
     </div>
 
     <div class="side-nav-bar">
@@ -91,25 +94,35 @@
     <section id="intro" class="scrollspy-section">
         <div class="main-slider">
             <div class="slider-item jarallax" data-speed="0.2">
-                <img src="images/main-banner1.jpg" alt="banner" class="jarallax-img">
+                <img src="images/top-view-photography-accesories-with-copy-space.jpg" alt="banner"
+                    class="jarallax-img">
                 <div class="banner-content">
-                    <h2 class="banner-title txt-fx">Modern Designs</h2>
+                    <h2 class="banner-title txt-fx">photography</h2>
 
                 </div><!--banner-content-->
             </div><!--slider-item-->
 
             <div class="slider-item jarallax" data-speed="0.2">
-                <img src="images/main-banner2.jpg" alt="banner" class="jarallax-img">
+                <img src="images/videography-specialist-editing-new-movie-clip-montage.jpg" alt="banner"
+                    class="jarallax-img">
                 <div class="banner-content">
-                    <h2 class="banner-title txt-fx">Clean & Minimal</h2>
+                    <h2 class="banner-title txt-fx">videography</h2>
+
+                </div><!--banner-content-->
+            </div><!--slider-item-->
+            <div class="slider-item jarallax" data-speed="0.2">
+                <img src="images/advertising.png" alt="banner" class="jarallax-img">
+                <div class="banner-content">
+                    <h2 class="banner-title txt-fx">advertising</h2>
 
                 </div><!--banner-content-->
             </div><!--slider-item-->
 
             <div class="slider-item jarallax" data-speed="0.2">
-                <img src="images/main-banner3.jpg" alt="banner" class="jarallax-img">
+                <img src="images/marketing-advertising-commercial-strategy-concept.jpg" alt="banner"
+                    class="jarallax-img">
                 <div class="banner-content">
-                    <h2 class="banner-title txt-fx">Personalize and Customize</h2>
+                    <h2 class="banner-title txt-fx">marketing</h2>
 
                 </div><!--banner-content-->
             </div><!--slider-item-->
@@ -145,7 +158,7 @@
 
                 <div class="col-md-6">
                     <figure class="jarallax-keep-img">
-                        <img src="images/singleimage.jpg" alt="about us" class="jarallax-img single-image">
+                        <img src="images/about.jpg" alt="about us" class="jarallax-img single-image">
                     </figure>
                 </div>
                 <div class="col-md-6 description text-lead">
@@ -219,8 +232,10 @@
                             class="img-fluid" alt="portfolio"></a>
                 </div>
                 <div class="col mb-4 portfolio-item videos">
-                    <div class="video-background" style="background-color: #000; padding: 20px; position: relative; overflow: hidden;">
-                        <a href="videos/zain aldean.mp4" class="video-link" title="Sample Caption goes here for Portfolio Item 6.">
+                    <div class="video-background"
+                        style="background-color: #000; padding: 20px; position: relative; overflow: hidden;">
+                        <a href="videos/zain aldean.mp4" class="video-link"
+                            title="Sample Caption goes here for Portfolio Item 6.">
                             <video class="img-fluid" controls autoplay width="100%" height="50%">
                                 <source src="videos/zain aldean.mp4" type="video/mp4">
                                 Your browser does not support the video tag.
@@ -228,7 +243,7 @@
                         </a>
                     </div>
                 </div>
-                
+
                 <div class="col mb-4 portfolio-item videos">
                     <a href="images/portfolio-large-1.jpg" class="image-link"
                         title="Sample Caption goes here for Portfolio Item 7."><img src="images/portfolio-thumb-7.jpg"
@@ -346,7 +361,8 @@
                     <div class="services-item">
                         <span class="number">02</span>
                         <h3>Social Media Management</h3>
-                        <p>Through The Most Importent Methods, Such As Using Marketing Strategies That Enable You To Reach
+                        <p>Through The Most Importent Methods, Such As Using Marketing Strategies That Enable You To
+                            Reach
                             The Largest Number Of Customers And Attract Their Attention.
                         </p>
                     </div>
@@ -357,8 +373,8 @@
                     <div class="services-item">
                         <span class="number">03</span>
                         <h3>Influencer Marketing</h3>
-                        <p>By Building Brand Awareness We Enhance It's Image Among Consumers And Also Simulate Purchase 
-                            In More Atractive Ways. 
+                        <p>By Building Brand Awareness We Enhance It's Image Among Consumers And Also Simulate Purchase
+                            In More Atractive Ways.
                         </p>
                     </div>
                 </div>
@@ -374,7 +390,8 @@
                     <div class="services-item">
                         <span class="number">05</span>
                         <h3>Content Creating And Marketing</h3>
-                        <p>We Help You Write Distinctive Content To Be Uninique In The Market And To Market And To Make Our Own
+                        <p>We Help You Write Distinctive Content To Be Uninique In The Market And To Market And To Make
+                            Our Own
                             Mark In Marketing Your Product
                         </p>
                     </div>
@@ -383,7 +400,7 @@
                     <div class="services-item">
                         <span class="number">06</span>
                         <h3>Website Design</h3>
-                        <p> 
+                        <p>
                         </p>
                     </div>
                 </div>
@@ -535,7 +552,8 @@
 
                         <p>If you have any question about our process or company? Let us know how we can help you.</p>
 
-                        <form id="form-contact" class="form-light" action="{{ route('contact.submit') }}" method="POST">
+                        <form id="form-contact" class="form-light" action="{{ route('contact.submit') }}"
+                            method="POST">
                             @csrf
                             <p>
                                 <input type="text" name="name" placeholder="Your Full Name*" required>
@@ -552,9 +570,10 @@
                                     <span>I agree to the privacy policy</span>
                                 </label>
                             </p>
-                            <button class="btn btn-accent btn-rounded btn-xlarge btn-full" type="submit">Submit</button>
+                            <button class="btn btn-accent btn-rounded btn-xlarge btn-full"
+                                type="submit">Submit</button>
                         </form>
-                        
+
 
 
                     </div>
@@ -565,28 +584,28 @@
                     <div class="right-content">
 
                         <div class="iconbox">
-                            <i class="icon icon-location"></i>
+                            <i class="icon icon-location" id="nav_icone"></i>
                             <div class="detail">
                                 <strong>Address </strong>
                                 <p>العقبة - دوار هيا - عمارة الملكية - الطابق الأول</p>
                             </div>
                         </div>
                         <div class="iconbox">
-                            <i class="icon icon-location2"></i>
+                            <i class="icon icon-location2" id="nav_icone"></i>
                             <div class="detail">
                                 {{-- <strong>Address 2:</strong> --}}
                                 <p>Aqaba-Haya Circle-RJ Building-1st Floor</p>
                             </div>
                         </div>
                         <div class="iconbox">
-                            <i class="icon icon-phone"></i>
+                            <i class="icon icon-phone" id="nav_icone"></i>
                             <div class="detail">
                                 <strong>Phone:</strong>
                                 <p>+962 79 084 6327</p>
                             </div>
                         </div>
                         <div class="iconbox">
-                            <i class="icon icon-mail3"></i>
+                            <i class="icon icon-mail3" id="nav_icone"></i>
                             <div class="detail">
                                 <strong>Email:</strong>
                                 <p><a href="mailto:Lanaalmanaseerr@gmail.com">Lanaalmanaseerr@gmail.com</a></p>
@@ -641,25 +660,27 @@
         </div>
     </section>
 
-    <footer id="footer">
+    <footer id="footer" class="footer_contaner">
         <div class="container">
             <div class="row">
 
-                <div class="col-md-6">
+                <div class="col-md-5">
 
                     <div class="footer-menu menu-item-01">
-                        <img src="images/logo.png" alt="logo" class="footer-logo">
- 
+                        <img src="images/logo-removebg-preview.png" alt="logo" class="footer-logo">
+
                         <div class="social-links">
-                            <p>Follow Us On</p>
+                            <p class="footer_tiltle">Follow Us On</p>
                             <ul>
                                 <li>
-                                    <a href="https://www.facebook.com/groups/482275915868920/" target="_blank"><i class="fa-brands fa-facebook-f"></i></a>
+                                    <a class="footer_tiltle" href="https://www.facebook.com/groups/482275915868920/"
+                                        target="_blank"><i class="fa-brands fa-facebook-f"></i></a>
                                 </li>
                                 <li>
-                                    <a href="https://www.instagram.com/all_about_aqabaa/" target="_blank"><i class="fab fa-instagram"></i></a>
+                                    <a class="footer_tiltle" href="https://www.instagram.com/all_about_aqabaa/"
+                                        target="_blank"><i class="fab fa-instagram"></i></a>
                                 </li>
-    
+
                             </ul>
                         </div>
                     </div>
@@ -669,19 +690,19 @@
                 <div class="col-md-2">
 
                     <div class="footer-menu menu-item-02">
-                        <h5>quick links</h5>
+                        <h5 class="footer_tiltle">quick links</h5>
                         <ul class="menu-list">
-                            <li class="menu-item">
-                                <a href="#">home</a>
+                            <li class="menu-item" id="social_link_footer">
+                                <a class="social_link_footer" href="#home">home</a>
                             </li>
                             <li class="menu-item">
-                                <a href="#about">about us </a>
+                                <a class="social_link_footer" href="#about">about us </a>
                             </li>
                             <li class="menu-item">
-                                <a href="#services">services</a>
+                                <a class="social_link_footer" href="#services">services</a>
                             </li>
                             <li class="menu-item">
-                                <a href="#our-work">Our Work</a>
+                                <a class="social_link_footer" href="#our-work">Our Work</a>
                             </li>
 
                             {{-- ??????????????????????????????/ --}}
@@ -690,7 +711,7 @@
                             </li> --}}
                             {{-- ?????????????????? --}}
                             <li class="menu-item">
-                                <a href="#contact">contact us</a>
+                                <a class="social_link_footer" href="#contact">contact us</a>
                             </li>
                         </ul>
                     </div>
@@ -700,19 +721,22 @@
                 <div class="col-md-4">
 
                     <div class="footer-menu menu-item-03">
-                        <h5>contact info</h5>
+                        <h5 class="footer_tiltle">contact info</h5>
                         <ul class="menu-list">
                             <li class="menu-item">
-                                <i class="icon icon-location"></i>العقبة - دوار هيا - عمارة الملكية - الطابق الأول
+                                <i class="icon icon-location" id="nav_icone"></i>العقبة - دوار هيا - عمارة الملكية -
+                                الطابق الأول
                             </li>
                             <li class="menu-item">
-                                <i class="icon icon-location2"></i>Aqaba-Haya Circle-RJ Building-1st Floor
+                                <i class="icon icon-location2" id="nav_icone"></i>Aqaba-Haya Circle-RJ Building-1st
+                                Floor
                             </li>
                             <li class="menu-item">
-                                <i class="icon icon-phone"></i>+962 79 084 6327
+                                <i class="icon icon-phone" id="nav_icone"></i>+962 79 084 6327
                             </li>
                             <li class="menu-item">
-                                <i class="icon icon-envelope-o"></i><a href="mailto:Lanaalmanaseerr@gmail.com"
+                                <i class="icon icon-envelope-o" id="nav_icone"></i><a
+                                    href="mailto:Lanaalmanaseerr@gmail.com"
                                     class="mail-id">Lanaalmanaseerr@gmail.com</a>
                             </li>
                         </ul>
@@ -720,17 +744,17 @@
 
                 </div>
 
- 
 
+
+            </div>
+        </div>
+        <div class="copyright">
+            <div class="copyright-text">
+                <p>© 2023. All Rights Reserved.</p>
             </div>
         </div>
     </footer>
 
-    <div class="copyright">
-        <div class="copyright-text">
-            <p>© 2023. All Rights Reserved.</p>
-        </div>
-    </div>
 
 
     <script src="js/jquery-1.11.0.min.js"></script>
